4e3e48d9abd5202874f3bd4589c88e9a259e6e78
Abadkova <159443184+Abadkova@users.noreply.github.com>
2024-03-12 23:40:51 -0800

    Add files via upload

---
+++ Задача3.cs
@@ -0,0 +1,22 @@
+﻿class ReverseArray
+{
+    static void PrintArrayReverse(int[] arr, int index)
+    {
+        if (index < 0 || index >= arr.Length)
+        {
+            return;
+        }
+
+        PrintArrayReverse(arr, index + 1);
+        Console.Write(arr[index] + " ");
+    }
+
+    static void Main()
+    {
+        int[] array = { 1, 2, 5, 0, 10, 34 };
+        Console.Write($"Array elements (reverse): ");
+        PrintArrayReverse(array, 0);
+    }
+}
+
+
